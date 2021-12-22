2f0a353c7d16f80da779d94dfb58fe639b0e495b
Lukasz Kielczewski <lukasz.kielczewski66@gmail.com>
2021-12-22 14:45:26 +0100

    update cars site

--- views/site/cars.php
+++ views/site/cars.php
@@ -6,3 +6,11 @@ use yii\helpers\Html;
 
 $this->title = 'Cars';
 $this->params['breadcrumbs'][] = $this->title;
+?>
+
+<main>
+    <section class="top-hero service-hero">
+        <div class="hero-shadow"></div>
+        <p class="top-hero-text text-light">Our cars</p>
+    </section>
+</main>
